b3f97a20431adf9160b174935018ab9aaa96de4c
prs-bhanu <prs.bhanu@gmail.com>
2024-03-08 01:50:47 +0530

    changed comments on previous programs

--- src/main/java/hf4/Clock.java
+++ src/main/java/hf4/Clock.java
@@ -1,6 +1,8 @@
 package hf4;
 public class Clock {
+    /* instance variable time is decleared */
     String time;
+    /* getters and setters are used to set the values */
     void setTime(String t) {
         time = t;
     }
@@ -9,10 +11,14 @@ public class Clock {
     }
 }
 class ClockTestDrive {
+    /* main method is decleared (main method can be decleared only once but as only one main method)*/
     public static void main(String[] args) {
+        /* object is created*/
         Clock c = new Clock();
-        c.setTime("4598");
+        /* getters and setters method are called */
+        c.setTime("0953");
         String tod = c.getTime();
+
         System.out.println("time: "+tod);
     }
 
--- src/main/java/hf4/Mix4.java
+++ src/main/java/hf4/Mix4.java
@@ -1,11 +1,15 @@
 package hf4;
 
 public class Mix4 {
+    /* instance variable is decleared */
     int counter =0;
+    /* main method */
     public static void main (String[] args) {
         int count = 0;
+        /* created an array of an object size */
         Mix4[] mixes = new Mix4[20];
         int i = 0;
+        /* while loop */
         while (i < 9) {
             mixes[i] = new Mix4();
             mixes[i].counter = mixes[i].counter + 1;

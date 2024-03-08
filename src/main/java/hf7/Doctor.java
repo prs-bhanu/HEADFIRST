7c7596392cf9a0285f947ab69c64fc9db57da8ba
prs-bhanu <prs.bhanu@gmail.com>
2024-03-08 22:33:57 +0530

    if there are more than one class will the classes will be created sepearately?

---
+++ src/main/java/hf7/Doctor.java
@@ -0,0 +1,9 @@
+package hf7;
+
+public class Doctor {
+    boolean worksAtHospaital;
+
+    void treatpatient() {
+        //perform checkup
+    }
+}
---
+++ src/main/java/hf7/FamilyDoctor.java
@@ -0,0 +1,14 @@
+package hf7;
+
+public class FamilyDoctor extends Doctor{
+    boolean makeHouseCalls;
+    void giveAdvice(){
+
+    }
+}
+public class Surgeone extends Doctor {
+    void treatpatient() {
+        void makeIncision() {
+        }
+    }
+}

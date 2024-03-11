0c108769b7353ce8abd2feedf401df4ec6892e6b
prs-bhanu <prs.bhanu@gmail.com>
2024-03-11 19:48:40 +0530

    commments are added.

--- src/main/java/hf7/Surgeone.java
+++ src/main/java/hf7/Surgeone.java
@@ -1,10 +1,13 @@
 package hf7;
+/*sub class of the DOCTOR super class*/
 class FamilyDoctor extends Doctor{
     boolean makeHouseCalls;
+    /* method as to give adivice from family doctor*/
     void giveAdvice(){
 
     }
 }
+/* sub class of DOCTOR */
 public class Surgeone extends Doctor {
     void treatpatient() {
     }

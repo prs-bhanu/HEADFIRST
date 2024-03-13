df1765aba3cead0aa644226367b6bf245fe3bb50
prs-bhanu <prs.bhanu@gmail.com>
2024-03-13 19:01:24 +0530

    testing the snippets

---
+++ src/main/java/hf10/Foo5.java
@@ -0,0 +1,42 @@
+package hf10;
+
+public class Foo5 {
+   /*
+        int x = 12;
+
+        public static void go(final int x) {
+            System.out.println(x);
+        }
+    }*/
+  /* static final int x = 12;
+
+    public void go(final int x) {
+        System.out.println(x);
+    }
+}*/
+  /* static final int x = 12;
+
+    public void go() {
+        System.out.println(x);
+    }
+}*/
+  /* static int x;
+
+    public void go() {
+        System.out.println(x);
+    }
+}*/
+   /*static int x;
+
+    public void go() {
+        System.out.println(x);
+    }
+}*/
+
+    //this code won't compile//
+   //*final/*
+   int x;
+    public void go() {
+        System.out.println(x);
+    }
+}
